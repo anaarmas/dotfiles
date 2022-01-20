5f048c0169013269be3eb7682d7e4762a243d2c6
anaarmas <54946499+anaarmas@users.noreply.github.com>
2022-01-20 14:56:38 +0100

    new alias to export a fresh oauth token

--- .gh_ts_dev.sh
+++ .gh_ts_dev.sh
@@ -5,6 +5,10 @@ alias enable_alert_issue_links="./bin/toggle-feature-flag enable task_list_beta;
 
 alias enable_code_scanning_in_db="./bin/rake --trace "enterprise:code_scanning:create[]";./bin/toggle-feature-flag enable advanced_security_private_beta;enable_issue_links"
 
+alias seed_with_turbotest="cd ~/Repos/github/turbotest;poetry run turbotest run --url http://github.localhost --organization github --cleanup skip main_flow/first_analysis/first_alert;echo 'remember to update development PAT AND enable GHAS for github org repos with new codespaces instances'"
+
+alias export_oauth_token="unset GITHUB_TOKEN;export GITHUB_TOKEN="$(gh config get --host github.com oauth_token)""
+
 export ENTERPRISE_ADVANCED_SECURITY_ENABLED=true # Only needed in Enterprise mode, but there's no disadvantage to setting it in DotCom mode too.
 export ENTERPRISE_CODE_SCANNING_ENABLED=true # Only needed in Enterprise mode, but there's no disadvantage to setting it in DotCom mode too.
 export TURBOSCAN_S3_ENDPOINT="http://localhost:9000"

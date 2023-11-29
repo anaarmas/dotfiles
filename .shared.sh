4de41b55dc0cefd250574418cba89eb98d3b9fe6
anaarmas <54946499+anaarmas@users.noreply.github.com>
2023-11-29 10:04:06 +0100

    iterm2 shell integration

--- .shared.sh
+++ .shared.sh
@@ -3,7 +3,7 @@ if test -f /Users/ana/.ssh/id_rsa_gh_prod; then
   ssh-add --apple-use-keychain /Users/ana/.ssh/id_rsa_gh_prod
 fi
 
-
+export EDITOR=vim
 
 eval $(/opt/homebrew/bin/brew shellenv)
 
@@ -38,3 +38,9 @@ PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
 export PATH
 
+############################
+# iterm2 shell integration
+############################
+# in a new machine this will require installing iterm2 and runnning
+#      > curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash 
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
